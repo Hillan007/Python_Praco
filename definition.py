72de3cb9d5bf7d3861ed92f7ca073adaa009a66f
Golden Champ <victorhillan007@gmail.com>
2026-06-23 11:50:45 +0300

    Add entrypoint to return.py

---
+++ definition.py
@@ -0,0 +1,14 @@
+def adding(a, b, c):
+    print(a, "+", b, "+", c, "=", a + b + c)
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Add three numbers and print the result.")
+    parser.add_argument('a', type=float, nargs='?', help='first number', default=1.0)
+    parser.add_argument('b', type=float, nargs='?', help='second number', default=2.0)
+    parser.add_argument('c', type=float, nargs='?', help='third number', default=3.0)
+    args = parser.parse_args()
+
+    adding(args.a, args.b, args.c)
+
---
+++ return.py
@@ -0,0 +1,13 @@
+def happy_new_year(wishes = True):
+    print("Three...")
+    print("Two...")
+    print("One...")
+    if not wishes:
+        return
+
+    print("Happy New Year!")
+
+
+if __name__ == "__main__":
+    happy_new_year()
+
