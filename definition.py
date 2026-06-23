5d9bdd882f3d30440e1dd03fc3fd875b23a7f9f1
Golden Champ <victorhillan007@gmail.com>
2026-06-23 12:08:06 +0300

    numbers change

--- definition.py
+++ definition.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser(description="Add three numbers and print the result.")
-    parser.add_argument('a', type=float, nargs='?', help='first number', default=1.0)
-    parser.add_argument('b', type=float, nargs='?', help='second number', default=2.0)
-    parser.add_argument('c', type=float, nargs='?', help='third number', default=3.0)
+    parser.add_argument('a', type=float, nargs='?', help='first number', default=3.0)
+    parser.add_argument('b', type=float, nargs='?', help='second number', default=4.0)
+    parser.add_argument('c', type=float, nargs='?', help='third number', default=5.0)
     args = parser.parse_args()
 
     adding(args.a, args.b, args.c)
