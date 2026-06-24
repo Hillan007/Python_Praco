8c85a7fde7fe414d26460395b40a5156723a3ef1
Golden Champ <victorhillan007@gmail.com>
2026-06-24 10:18:07 +0300

    function1

---
+++ function1.py
@@ -0,0 +1,38 @@
+def is_year_leap(year):
+    #
+    # Your code from the previous LAB.
+    #
+    if year % 4 != 0:
+        return False
+    if year % 100 != 0:
+        return True
+    if year % 400 != 0:
+        return False
+    return True
+
+def days_in_month(year, month):
+    #
+    # Write your new code here.
+    #
+    if month in (1, 3, 5, 7, 8, 10, 12):
+        return 31
+    if month in (4, 6, 9, 11):
+        return 30
+    if month == 2:
+        if is_year_leap(year):
+            return 29
+        return 28
+    return None
+
+test_years = [1900, 2000, 2016, 1987]
+test_months = [2, 2, 1, 11]
+test_results = [28, 29, 31, 30]
+for i in range(len(test_years)):
+    yr = test_years[i]
+    mo = test_months[i]
+    print(yr, mo, "->", end="")
+    result = days_in_month(yr, mo)
+    if result == test_results[i]:
+        print("OK")
+    else:
+        print("Failed")
